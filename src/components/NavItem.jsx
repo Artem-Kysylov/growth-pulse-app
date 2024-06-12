1cef4ff3a51c78747813e4a6c2e6a69181d15413
kysyl <kysylyov.artem@gmail.com>
2024-06-12 10:15:38 +0300

    finish with sidebar and add tremor library

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -4,11 +4,18 @@ import { NavLink } from "react-router-dom"
 
 export const NavItem = ({ icon: Icon, title, path }) => {
   return (
-    <NavLink to={path}>
-      <div className='transition ease-in-out duration-500 flex items-center gap-[8px] p-[10px] cursor-pointer hover:bg-primary hover:w-full hover:rounded-[5px] group'>
-          <Icon className='transition ease-in-out duration-500 text-transparentBlack group-hover:text-white'></Icon> 
-          <p className='transition ease-in-out duration-500 text-[14px] font-medium text-transparentBlack group-hover:text-white'>{title}</p>
-      </div>
+    <NavLink to={path}
+    className={({ isActive }) =>
+      isActive ? 'active nav-item' : 'nav-item'
+    }
+    >
+      {({ isActive }) => (
+        <div className={`relative transition ease-in-out duration-500 flex items-center gap-[8px] w-[150px] p-[10px] cursor-pointer hover:bg-primary hover:w-[150px] hover:rounded-[5px] group ${isActive ? 'text-primary' : ''}`}>
+          <Icon className={`transition ease-in-out duration-500 ${isActive ? 'text-green-500' : 'text-transparentBlack'} group-hover:text-white`} />
+          <p className={`transition ease-in-out duration-500 text-[14px] font-medium ${isActive ? 'text-green-500' : 'text-transparentBlack'} group-hover:text-white`}>{title}</p>
+          {isActive && <div className="absolute right-0 h-[24px] w-[3px] rounded bg-primary"></div>}
+        </div>
+      )}
     </NavLink>
   )
 }
--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,10 +1,15 @@
 // Imports 
 import React from 'react'
+import { AreaChart } from '@tremor/react'
 
 export const Dashboard = () => {
   return (
-    <div className='w-full h-full bg-bg'>
-      Dashboard
+    <div className='grid grid-cols-2 gap-5 w-full h-full bg-bg overflow-y-scroll'>
+      <div className='flex flex-col gap-5'>
+        <AreaChart/>
+      </div>
+
+      <div className='flex flex-col gap-5'></div>
     </div>
   )
 }
