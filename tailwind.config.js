6bce4b29ae8689a15b9977b4b682396edae5672f
kysyl <kysylyov.artem@gmail.com>
2024-09-23 20:06:43 +0300

    update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,17 @@
+import { light, dark } from './src/colors/colors'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-
-    // Path to Tremor module
-    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      bg: '#F2F2F2',
-      white: '#FFFFFF',
-      grey: '#E7E7E7',
-      text: '#3B433F',
-      primary: '#40A578',
-      primaryHover: '#2D7454',
-      transparentBlack: 'rgba(59, 67, 63, 0.5)',
+    extend: {
+      colors: {
+        light,
+        dark,
+      },
     },
     screens: {
       'phone': '400px',      
@@ -23,7 +19,6 @@ export default {
       'laptop': '1280px',
       'desktop': '1600px',
     },
-    extend: {},
   },
   plugins: [],
 }
