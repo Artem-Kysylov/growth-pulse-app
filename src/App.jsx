aed944ffa457542c553d51e642248fb65fccb08f
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2025-08-26 16:14:14 +0300

    fixing routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // Imports 
-import {createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Outlet, RouterProvider, Navigate } from 'react-router-dom'
 import { SignedIn, SignedOut } from "@clerk/clerk-react"
 import './index.css'
 
@@ -19,25 +19,35 @@ import { NotFound } from './routes/NotFound.jsx'
 
 const AppLayout = () => {
   return (
-    <SignedIn>
-      <div className='flex h-screen'>
-        <SideBar/>
-        <div className='flex flex-col w-full h-full overflow-x-auto'>
-          <TopBar/>
-          <div className='flex-1 bg-background-light dark:bg-background-dark overflow-y-auto transition ease-in-out duration-500'>
+    <>
+      <SignedIn>
+        <div className='flex h-screen'>
+          <SideBar/>
+          <div className='flex flex-col w-full h-full overflow-x-auto'>
+            <TopBar/>
+            <div className='flex-1 bg-background-light dark:bg-background-dark overflow-y-auto transition ease-in-out duration-500'>
               <Outlet/>
+            </div>
           </div>
         </div>
-      </div>
-    </SignedIn>
+      </SignedIn>
+      <SignedOut>
+        <Navigate to="/signin" replace />
+      </SignedOut>
+    </>
   )
 }
 
 const AppEntry = () => {
   return (
-    <SignedOut>
-      <Outlet/>
-    </SignedOut>
+    <>
+      <SignedOut>
+        <Outlet/>
+      </SignedOut>
+      <SignedIn>
+        <Navigate to="/" replace />
+      </SignedIn>
+    </>
   )
 }
 
